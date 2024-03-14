b13c888a89e27720853ca16c4fa5aafed431aa83
azadaliyev44 <68191048+azadaliyev44@users.noreply.github.com>
2024-03-14 18:02:31 +0400

    Update test_methods.py

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,23 +1,10 @@
 import unittest
-import pygame
 from src.engine.game_scene import GameScene
 from src.entity.entity import Entity
 from src.entity.enemy import Enemy
 from src.entity.bullet import Bullet
 
 class TestGameScene(unittest.TestCase):
-    def setUp(self):
-        pygame.init()
-        self.background = (0, 0, 0)
-        self.win_width = 800
-        self.win_height = 600
-        self.pygame = pygame
-        self.is_running = [True]
-        self.scene = GameScene(self.background, self.win_width, self.win_height, self.pygame, self.is_running)
-
-    def tearDown(self):
-        pygame.quit()
-
     def test_entity_fields(self):
         entity = Entity({"x":300,"y":300}, 6, 100, {"width":300,"height":300}, "assets/images/enemy_boss.png", None, "nenon")
         entity.set_health(1)
